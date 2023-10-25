c528adb30f5e3dbe1cf34497610b1af192e15d41
Maxim <therikomero@gmail.com>
2023-10-25 14:51:56 +0700

    complete task1

---
+++ src/main/java/edu/hw3/Task1.java
@@ -0,0 +1,31 @@
+package edu.hw3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task1 {
+    private static Map<Character, Character> lookupTable;
+    private Task1() { }
+
+    static {
+        lookupTable = new HashMap<>();
+        for (int i = (int) 'a', counter = 0; i <= (int) 'z'; i++, counter++) {
+            lookupTable.put((char) i, (char) ((int)'z' - counter));
+            lookupTable.put((char) (i + ((int)'A' - (int)'a')), (char) ((int)'z' - counter + ((int)'A' - (int)'a')));
+        }
+    }
+
+    public static String atbash(String string) {
+        StringBuilder result = new StringBuilder();
+        char[] stringCharArray = string.toCharArray();
+        for(char letter: stringCharArray) {
+            if (lookupTable.containsKey(letter)){
+                result.append(lookupTable.get(letter));
+            }
+            else {
+                result.append(letter);
+            }
+        }
+        return result.toString();
+    }
+}
