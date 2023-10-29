a032e6abb41564dc579fcdf93c663cf174b25ec0
Maxim <therikomero@gmail.com>
2023-10-29 21:32:21 +0700

    fix style issue

--- src/main/java/edu/hw3/Task1.java
+++ src/main/java/edu/hw3/Task1.java
@@ -4,16 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Task1 {
-    private static final Map<Character, Character> lookupTable;
+    private static final Map<Character, Character> LOOKUP_TABLE;
 
     private Task1() {
     }
 
     static {
-        lookupTable = new HashMap<>();
+        LOOKUP_TABLE = new HashMap<>();
         for (int i = (int) 'a', counter = 0; i <= (int) 'z'; i++, counter++) {
-            lookupTable.put((char) i, (char) ((int) 'z' - counter));
-            lookupTable.put(
+            LOOKUP_TABLE.put((char) i, (char) ((int) 'z' - counter));
+            LOOKUP_TABLE.put(
                 (char) (i + ((int) 'A' - (int) 'a')),
                 (char) ((int) 'z' - counter + ((int) 'A' - (int) 'a'))
             );
@@ -24,8 +24,8 @@ public class Task1 {
         StringBuilder result = new StringBuilder();
         char[] stringCharArray = string.toCharArray();
         for (char letter : stringCharArray) {
-            if (lookupTable.containsKey(letter)) {
-                result.append(lookupTable.get(letter));
+            if (LOOKUP_TABLE.containsKey(letter)) {
+                result.append(LOOKUP_TABLE.get(letter));
             } else {
                 result.append(letter);
             }
